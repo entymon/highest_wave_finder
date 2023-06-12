61dba7be373808e0ac3f95174d6b7aeac59dd677
anymouse_tsh <pawel.olejniczak@tsh.io>
2023-06-12 23:31:11 +0200

    working on xarrays

--- app/api.py
+++ app/api.py
@@ -1,12 +1,13 @@
 from typing import Union
 import folium
-from folium.features import LatLngPopup
+import xarray as xr
 
+from jinja2 import Template
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from jinja2 import Template
+from folium.features import LatLngPopup
 
 app = FastAPI()
 
@@ -18,6 +19,7 @@ class GetLatLngPopup(LatLngPopup):
             {% macro script(this, kwargs) %}
                 var {{this.get_name()}} = L.popup();
                 function latLngPop(e) {
+                    window.location="/?lat=" + e.latlng.lat.toFixed(7) + "&lng=" + e.latlng.lng.toFixed(7);
                     {{this.get_name()}}
                         .setLatLng(e.latlng)
                         .setContent("Latitude: " + e.latlng.lat.toFixed(7) +
@@ -39,11 +41,25 @@ def generate_map(lat: float = 53.073635, lng: float = 8.806422):
 
     GetLatLngPopup().add_to(m)
 
+    folium.Marker(
+        location=[lat, lng],
+        popup="Latitude: " + str(lat) + "; Longitude: " + str(lng) + "",
+    ).add_to(m)
+
     m.save("templates/map.html")
     map_html = m._repr_html_()
     return {
         "map": map_html
     }
+
+def get_highest_wave(lat: float = 53.073635, lng: float = 8.80):
+
+    ds = xr.open_dataset("waves_data/waves_2019-01-01.nc")
+    # xr.load_dataset("waves_data/waves_2019-01-01.nc")
+
+    print(ds.hmax.query(longitude=str(lat), latitude=str(lng)))
+
+# get_highest_wave()
 
 @app.get("/hello", response_class=HTMLResponse)
 def read_root():
@@ -59,7 +75,10 @@ async def read_item(request: Request, lat: str = '0', lng: str = ''):
         "longitude": lng
     }
 
-
+    # if lat != '0':
+    #     get_highest_wave(lat, lng)
 
     generate_map(float(lat), float(lng))
     return templates.TemplateResponse("index.html", template_data)    
+
+
