b682bdd436eb1c86d562db87e6452ee21c345502
anymouse_tsh <pawel.olejniczak@tsh.io>
2023-06-12 22:23:48 +0200

    tidy up urls

--- app/api.py
+++ app/api.py
@@ -22,7 +22,7 @@ class GetLatLngPopup(LatLngPopup):
                         .setLatLng(e.latlng)
                         .setContent("Latitude: " + e.latlng.lat.toFixed(7) +
                                     "<br>Longitude: " + e.latlng.lng.toFixed(7) +
-                                    "<br><a href='/test?lat=" + e.latlng.lat.toFixed(7) + "&lng=" + e.latlng.lng.toFixed(7) + "'>test</a>")
+                                    "<br><a href='/?lat=" + e.latlng.lat.toFixed(7) + "&lng=" + e.latlng.lng.toFixed(7) + "'>get wave</a>")
                         .openOn({{this._parent.get_name()}});
                     }
                 {{this._parent.get_name()}}.on('click', latLngPop);
@@ -33,7 +33,7 @@ class GetLatLngPopup(LatLngPopup):
         super(GetLatLngPopup, self).__init__()
         self._name = 'GetLatLngPopup'
     
-def generateMap():
+def generate_map():
 
     m = folium.Map(location=[53.073635, 8.806422], zoom_start=15, tiles='Stamen Terrain')
     # m.add_child(folium.LatLngPopup())
@@ -46,13 +46,13 @@ def generateMap():
         "map": map_html
     }
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/hello", response_class=HTMLResponse)
 def read_root():
     return {
         "hello": "world"
     }
 
-@app.get("/map", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
-    generateMap()
+    generate_map()
     return templates.TemplateResponse("index.html", {"request": request })    
